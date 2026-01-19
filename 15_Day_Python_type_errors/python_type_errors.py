d7e7270ca1b6913fb9d6965dcd54a06f17d7efd5
Moyosore Afolabi <afolabimoyosore54@gmail.com>
2026-01-19 17:58:47 +0100

    Day 15 completed

---
+++ 15_Day_Python_type_errors/python_type_errors.py
@@ -0,0 +1,13 @@
+# types of errors in Python
+# 1. syntax error
+# 2. Name error
+# 3. Index error
+# 4. module not found error
+# 5. attribute error
+# 6 key error
+# 7. type error
+# 8. import error
+# 9. value error
+# 10. zero division error
+
+print("hello world")
